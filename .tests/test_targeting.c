15d05daab5dddfb0ce10fac7e513eedbc3927a27
tpolonen <merirosvo@kyy.ninja>
2022-12-02 09:57:22 +0200

    Merge with remote

--- .tests/test_targeting.c
+++ .tests/test_targeting.c
@@ -5,8 +5,11 @@ int main(void)
 {
 	t_data		*test_data;
 	t_dintarr	*shape = get_enemy_shape();
-	int			*values;
+	t_values	*values;
 	t_strat		*strat = get_strat();
+	char		*output;
+	char		*combined_board;
+	size_t		board_size;
 
 	test_data = get_data();
 	printf("Enter when ready...\n");
@@ -15,24 +18,25 @@ int main(void)
 	test_data->player = 'x';
 	init_data(test_data);
 	ft_getline(0, &test_data->temp);
-	printf("shape len = %zu, alloced = %zu\n", shape->len, shape->alloced);
 	while (ft_strncmp(test_data->temp, "000", 3) != 0)
 	{
 		ft_memdel((void **)&test_data->temp);
 		ft_getline(0, &(test_data->temp));
 	}
 	can_read_board(test_data, shape);
-	printf("shape->len = %zu, shape->arr = %p\n", shape->len, shape->arr);
+	board_size = get_data()->width * get_data()->height;
+	combined_board = (char *)xalloc(board_size);
+	for (size_t i = 0; i < board_size; i++)
+		combined_board[i] = (strat->enemy[i] | strat->player[i]) != 0;
+	values = get_values(combined_board, shape);
+	output = xalloc(board_size);
 	for (size_t i = 0; i < shape->len; i++)
-	{
-		printf("%2zu/%zu = %d\n", i, shape->len, shape->arr[i]);
-	}
-	values = get_values(test_data->oboard_ptr, shape);
-	for (size_t i = 0; i < shape->len; i++)
-		test_data->oboard_ptr[shape->arr[i]] = '0' + (char)values[i];
+		output[shape->arr[i]] = '0' + (char)values->arr[i];
+	ft_memdel((void **)&values->arr);
 	find_new_target(strat);
 	for (size_t i = 0; i < strat->target->len; i++)
-		test_data->oboard_ptr[strat->target->arr[i]] = 'T';
-	debug_print(test_data->oboard_ptr, test_data->width, test_data->height);
+		output[strat->target->arr[i]] = 'T';
+	ft_putendl("Result:");
+	debug_print(output, test_data->width, test_data->height);
 	clean_exit(test_data, "Done", 0);
 }
